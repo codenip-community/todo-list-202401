abed617c698350ba5699c3cb74dfdd625d09d6cf
Adrián Ocaña <adrian.ocana01@gmail.com>
2024-01-15 21:12:12 +0100

    feat(router-component): create router

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($_SERVER['SERVER_PORT'] == 8080 && $uri === '/') {
-    echo 'Hello world';
-} elseif ($_SERVER['SERVER_PORT'] == 8000 && $uri === '/') {
-    readfile('index.html');
-} else {
+include_once('request.php');
+
+$request = getCurrentRequest();
+
+$routes = [
+    '/'     => [
+        'GET'  => 'getHome',
+        'POST' => 'postHome'
+    ],
+    '/ping' => [
+        'GET' => 'getPing'
+    ],
+];
+
+
+if (!array_key_exists($request['path'], $routes)) {
     http_response_code(404);
-    echo 'Not Found';
+    return 'Not Found';
+}
+
+
+$routePath = $routes[$request['path']];
+
+if (!array_key_exists($request['method'], $routePath)) {
+    http_response_code(405);
+} else {
+    call_user_func($routePath[$request['method']], $request);
+}
+
+
+
+function getHome(array $request)
+{
+    echo 'Hello world';
+}
+
+function postHome(array $request)
+{
+    echo json_encode(_queryData($request['method']));
+}
+
+function getPing(array $request)
+{
+    echo 'pong';
 }
